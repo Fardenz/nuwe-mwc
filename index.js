89f876bf204adffe7e532d9d3cb5b6ee0282b203
Javi Vañó <javibodi@gmail.com>
2022-02-28 16:52:03 +0100

    crud basico

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const vorpal = require('vorpal')();
+const express = require('express');
+const app = express();
 
 const { getDbConnection } = require('./src/db');
 
+app.get('/company', function (req, res) {
+    const sequelize = await getDbConnection();
+    const companies = await sequelize.models.company.findAll({
+        raw: true,
+    });
+    return companies;
+});
+
+app.post('/company', function (req, res) {
+    const sequelize = await getDbConnection();
+    await sequelize.models.company.create(req.data);
+});
+
+app.delete('/company', function (req, res) {});
+
+app.put('/company', function (req, res) {});
+
+app.listen(3000);
