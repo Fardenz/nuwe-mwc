9c801b0ab2eefe75e5c758c374b9467fc3959a6d
Javi Vañó <javibodi@gmail.com>
2022-03-01 12:08:52 +0100

    dividing uri

--- src/db/index.js
+++ src/db/index.js
@@ -20,15 +20,21 @@ async function initializeDbModels() {
     const uri = process.env.DATABASE_URL || 'postgres://mwc:changeme@localhost:5432/mwc';
     const ssl = process.env.DATABASE_URL && true;
     console.log(ssl, uri);
+    const username = uri.split(':')[1].slice(2);
+    const password = uri.split('@')[0].split(':')[2];
+    const host = uri.split('@')[1].split(':')[0];
+    const port = uri.split('@')[1].split(':')[1].split('/')[0];
+    const database = uri.split('@')[1].split(':')[1].split('/')[1];
+
     sequelize = new Sequelize(uri, {
-        /* dialect: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'mwc',
-        database: 'mwc',
-        password: 'changeme', */
+        dialect: 'postgres',
+        host: host || 'localhost',
+        port: port || 5432,
+        username: username || 'mwc',
+        database: database || 'mwc',
+        password: password || 'changeme',
         logging: log.debug.bind(log),
-        ssl: ssls,
+        ssl: ssl,
     });
 
     const modelDefiners = [require('../models/Company.model'), require('../models/Flight.model')];
